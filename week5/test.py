fcfab6498eec21a37135decc4c46e0abf8d278f3
Filipe <filipejvmf@gmail.com>
2024-10-26 15:29:41 -0300

    feat: week5 test

--- week5/test.py
+++ week5/test.py
@@ -1,5 +1,5 @@
 t1, t2, n = [int(i) for i in input().split()]
-print(t1, t2, n)
+
 for i in range(3, n + 1):
   k = t2
   t2 = t1 + t2 * t2
@@ -9,6 +9,5 @@ ans = ''
 while t2:
   ans = str(t2 % 10) + ans
   t2 //= 10
-
 
 print(ans)
